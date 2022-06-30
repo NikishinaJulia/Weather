7661bd0387ae9dd631bfb36007f39f3a699a71dd
Julia Nikishina <pandora55592@mail.ru>
2022-06-30 18:15:29 +0300

    Настроили view+viewmodel

--- MainActivity.kt
+++ MainActivity.kt
@@ -2,10 +2,20 @@ package ru.gb.weather
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.gb.weather.databinding.ActivityMainBinding
+import ru.gb.weather.view.weatherlist.WeatherListFragment
 
-class MainActivity : AppCompatActivity() {
+internal class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         setContentView(R.layout.activity_main)
+
+        if(savedInstanceState == null){
+            supportFragmentManager.beginTransaction().replace(R.id.container, WeatherListFragment.newInstance()).commit()
+        }
     }
 }
---
+++ view/weatherlist/WeatherListViewModel.kt
@@ -0,0 +1,22 @@
+package ru.gb.weather.view.weatherlist
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import ru.gb.weather.viewmodel.AppState
+import java.lang.Thread.sleep
+
+class WeatherListViewModel(val liveData:MutableLiveData<AppState> = MutableLiveData<AppState>()):ViewModel() {
+
+    fun sentRequest() {
+
+        liveData.value = AppState.Loading
+        Thread{
+            sleep(2000L)
+            liveData.postValue(AppState.Success(Any()))
+        }.start()
+
+
+
+    }
+
+}
