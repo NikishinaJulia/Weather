01b220773a2c7cafbc2f4da1b01fbc858548b3c7
Julia Nikishina <pandora55592@mail.ru>
2022-07-09 15:47:44 +0300

    Улучшили решение борьбы с утечкой памяти внутри WeatherListFragment.kt

--- app/src/main/java/ru/gb/weather/viewmodel/WeatherListFragment.kt
+++ app/src/main/java/ru/gb/weather/viewmodel/WeatherListFragment.kt
@@ -14,12 +14,16 @@ class WeatherListFragment : Fragment() {
         fun newInstance() = WeatherListFragment()
     }
 
-    private var binding : FragmentWeatherListBinding? = null
+    private var _binding : FragmentWeatherListBinding? = null
+    private val binding : FragmentWeatherListBinding
+    get(){
+        return _binding!!
+    }
 
 
     override fun onDestroy() {
         super.onDestroy()
-        binding = null
+        _binding = null
     }
 
 
@@ -31,8 +35,8 @@ class WeatherListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentWeatherListBinding.inflate(inflater)
-        return binding!!.root
+        _binding = FragmentWeatherListBinding.inflate(inflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -50,28 +54,28 @@ class WeatherListFragment : Fragment() {
     private fun renderData(appState: AppState) {
         when(appState) {
             is AppState.Error -> {
-                binding!!.loadingLayout.visibility = View.GONE
+                binding.loadingLayout.visibility = View.GONE
                 val result = appState.error.message
-                binding!!.cityName.text = result
-                binding!!.temperatureLabel.visibility = View.INVISIBLE
-                binding!!.feelsLikeLabel.visibility = View.INVISIBLE
-                binding!!.temperatureValue.text = ""
-                binding!!.feelsLikeValue.text = ""
-                binding!!.cityCoordinates.text = ""
+                binding.cityName.text = result
+                binding.temperatureLabel.visibility = View.INVISIBLE
+                binding.feelsLikeLabel.visibility = View.INVISIBLE
+                binding.temperatureValue.text = ""
+                binding.feelsLikeValue.text = ""
+                binding.cityCoordinates.text = ""
 
                  }
             AppState.Loading -> {
-                binding!!.loadingLayout.visibility = View.VISIBLE
+                binding.loadingLayout.visibility = View.VISIBLE
                  }
             is AppState.Success -> {
                 val result = appState.weatherData
-                binding!!.cityName.text = result.city.name
-                binding!!.temperatureValue.text = result.temperature.toString()
-                binding!!.feelsLikeValue.text = result.feelsLike.toString()
-                binding!!.cityCoordinates.text = "${result.city.lat}/${result.city.lon}"
-                binding!!.loadingLayout.visibility = View.GONE
-                binding!!.temperatureLabel.visibility = View.VISIBLE
-                binding!!.feelsLikeLabel.visibility = View.VISIBLE
+                binding.cityName.text = result.city.name
+                binding.temperatureValue.text = result.temperature.toString()
+                binding.feelsLikeValue.text = result.feelsLike.toString()
+                binding.cityCoordinates.text = "${result.city.lat}/${result.city.lon}"
+                binding.loadingLayout.visibility = View.GONE
+                binding.temperatureLabel.visibility = View.VISIBLE
+                binding.feelsLikeLabel.visibility = View.VISIBLE
 
             }
         }
